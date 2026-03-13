8be3ac0cb551b74999c8949683ed27eca4d834cc
Beenyameen <beenyameen@hotmail.com>
2026-03-13 15:44:37 +0000

    broker

---
+++ broker/broker.py
@@ -0,0 +1,26 @@
+import socket
+import zmq
+import rvl
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+sock.bind(("0.0.0.0", 10000))
+
+while True:
+    data = sock.recvfrom(1024)[0].decode()
+    if data.startswith("PI:"):
+        ip = data.split(":")[1]
+        break
+
+ctx = zmq.Context()
+sub = ctx.socket(zmq.SUB)
+sub.setsockopt(zmq.SUBSCRIBE, b"")
+sub.connect(f"tcp://{ip}:11000")
+
+pub = ctx.socket(zmq.PUB)
+pub.bind("tcp://0.0.0.0:12000")
+
+while True:
+    m = sub.recv_multipart()
+    pub.send_multipart([m[0], m[1], rvl.decompress(m[2])])
--- drone/ip.py
+++ drone/ip.py
@@ -1,9 +1,14 @@
 import socket
 import time
+import subprocess
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
 
 while True:
-    sock.sendto(b"PI", ("255.255.255.255", 10000))
+    try:
+        ip = subprocess.check_output(["hostname", "-I"]).decode().split()[0]
+        sock.sendto(f"PI:{ip}".encode(), ("255.255.255.255", 10000))
+    except Exception:
+        pass
     time.sleep(1)
